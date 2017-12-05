05c9aff2be9d54c3dd6c5b72c5460ff95c941c08
Kelly Wang <Kelly@Kellys-MBP.home>
2017-12-04 23:35:36 -0500

    add more projects

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,35 +21,44 @@ $(document).ready(function(){
       var $this = $(this);
       $('.filter').removeClass('active');
       $this.addClass('active');
-      $('.film, .interactive, .art').addClass('hide');
+      $('.film, .interactive, .art, .paint').addClass('hide');
       $('.code').removeClass('hide');
     });
 
-    // Show urban
+    // Show film
     $('.filter-film').on('click', function() {
       var $this = $(this);
       $('.filter').removeClass('active');
       $this.addClass('active');
-      $('.code, .interactive, .art').addClass('hide');
+      $('.code, .interactive, .art, .paint').addClass('hide');
       $('.film').removeClass('hide');
     });
 
-    // Show portrait
+    // Show interactive
     $('.filter-interactive').on('click', function() {
       var $this = $(this);
       $('.filter').removeClass('active');
       $this.addClass('active');
-      $('.code, .film, .art').addClass('hide');
+      $('.code, .film, .art, .paint').addClass('hide');
       $('.interactive').removeClass('hide');
     });
 
-    // Show portrait
+    // Show art
     $('.filter-art').on('click', function() {
       var $this = $(this);
       $('.filter').removeClass('active');
       $this.addClass('active');
-      $('.code, .film, .interactive').addClass('hide');
+      $('.code, .film, .interactive, .paint').addClass('hide');
       $('.art').removeClass('hide');
+    });
+
+    // Show paintings
+    $('.filter-paint').on('click', function() {
+      var $this = $(this);
+      $('.filter').removeClass('active');
+      $this.addClass('active');
+      $('.code, .film, .interactive, .art').addClass('hide');
+      $('.paint').removeClass('hide');
     });
 
 });
